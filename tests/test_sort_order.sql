8aa561f1d23a06ba4806b99dcd061d52d72cdb66
Vitaliy Demidov <vital.letter@gmail.com>
2024-02-18 18:32:03 -0800

    minor change

--- tests/test_sort_order.sql
+++ tests/test_sort_order.sql
@@ -1,3 +1,18 @@
+-- This file is part of Project recipe/mysql-semver, a program for working with Semantic Versions in MySQL.
+-- Copyright (C) 2024  Vitaliy Demidov
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License Version 3 as
+-- published by the Free Software Foundation.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 TRUNCATE `versions`;
 
 INSERT `versions` (`version`, `sort_order`)
